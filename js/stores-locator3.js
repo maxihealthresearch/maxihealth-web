99279315b3514c23c536176f3748da96ed6e76a1
dmitry mardukhayev <dmitrymar@yahoo.com>
2013-02-06 15:17:16 -0500

    latest commit

---
+++ js/stores-locator3.js
@@ -0,0 +1,109 @@
+jQuery.noConflict();
+
+function mapinit() {
+	var address = "1309 Avenue U, Brooklyn, NY";
+	geocoder = new google.maps.Geocoder();
+
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+
+			var myOptions = {
+				zoom: 15,
+				center: results[0].geometry.location,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+
+			var marker = new google.maps.Marker({
+				map: map, 
+				position: results[0].geometry.location
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
+	
+}
+
+function showMap(address) {
+	geocoder = new google.maps.Geocoder();
+
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+
+			var myOptions = {
+				zoom: 15,
+				center: results[0].geometry.location,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+
+			var marker = new google.maps.Marker({
+				map: map, 
+				position: results[0].geometry.location
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
+}
+
+// Put all your jquery code in your document ready area to avoid conflict with prototype
+jQuery(document).ready(function($) {
+
+mapinit();
+
+//this function displays list of stores using inputs(search) parameter
+var outputStores = function(inputs) {
+
+		var jqxhr = $.getJSON("http://maxihealth.com/ajax/stores-locator4.json", {
+			location: inputs
+		}, function(data) {
+
+// if stores found then execute this code block
+if (data.total_target_stores > 0) {
+
+//hide error message if it pops
+$('.stores_locations_body .msg_error').hide();
+
+// Display number of stores found
+$('.stores-found h2 span').prepend(data.total_target_stores);
+
+//use mustache.js to display all found stores
+    var template = $('#listTpl').html();
+    var html = Mustache.to_html(template, data);
+    $('#stores_list').html(html);
+//
+
+if (data.nearby_stores.length > 0) {
+
+//if nearby stores are found then display nearby title
+	$('.nearby-title').show();
+}
+
+	} else {
+	//Display message if 0 stores found
+	$('#stores_list').empty();
+	$('.stores_locations_body .msg_error').show();
+	}
+			
+		}).error(function() {
+			alert("Error connecting to Maxihealth stores database");
+		});
+	
+}
+
+
+//When page loads initially it displays default location
+outputStores('1309 Avenue U');
+
+$('#search_location_form').submit(function(evt) {
+		evt.preventDefault();
+
+		var inputs = $('#location').val();
+outputStores(inputs);
+
+	});
+
+
+});
