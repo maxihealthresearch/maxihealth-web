a44623ac47708b60f9c71a692ff176356cd45f1e
dmitry mardukhayev <dmitrymar@yahoo.com>
2013-03-07 12:37:02 -0500

    added jquery to manipulate image delete

--- admin/sections/products-features/edit.php
+++ admin/sections/products-features/edit.php
@@ -11,6 +11,7 @@
 <h2><?php echo ($id ? 'Edit' : 'Add') ?> product feature</h2>
 <form method="post" action="#" enctype="multipart/form-data">
 <input type="hidden" name="action" value="save" />
+<input type="hidden" name="deleteimage" id="deleteImageField" value="0" />
 <input type="hidden" name="id" value="<?php echo $r['id'] ?>" />
 <table>
 	<tr><th>Name:</th><td><input type="text" name="name" value="<?php echo $r['name'] ?>" size="30" class="text" /></td></tr>
@@ -22,16 +23,21 @@
 	<?php if ($id > 0) {?>
 	<tr><th>Current image:</th><td style="padding-left: 3px;">
 			<?php
+			echo '<p id="currentImage">';
 			$image_file = DIR_IMAGES_FEATURES.DIRECTORY_SEPARATOR.$r['id'].'.png';
 			if (file_exists($image_file)) {
 				list($w, $h) = getimagesize($image_file);
 				$size = round(filesize($image_file)/1024);
-				echo '<img alt="'.$r['name'].'" src="', WEB_DIR_IMAGES_FEATURES, $r['id'].'.png?dateStamp=', time(), '" />';
-				echo "$w x $h pixles, $size KiB";
+				echo '<img alt="'.$r['name'].'" src="', WEB_DIR_IMAGES_FEATURES, $r['id'].'.png?dateStamp=', time(), '" />&nbsp;&nbsp;';
+				echo "$w x $h pixles, $size KiB&nbsp;&nbsp;";
+				echo '<a href="#" id="deleteImageLink">delete image</a>';
+			echo '</p>';
 			}
-			else
-				echo '<em>- none -</em>';
+			else {
+			echo '<em>- none -</em>';
+			}
 			?>
+            <p id="noImageSelected" style="display:none;"><em>- none -</em></p>
 		</td>
 	</tr>
 	<?php } ?>
@@ -46,3 +52,25 @@
 </table>
 
 </form>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>
+     jQuery.noConflict();
+     
+     // Put all your jquery code in your document ready area to avoid conflict with prototype
+     jQuery(document).ready(function($){
+		 
+
+
+$( "#deleteImageLink" ).click(function(e) {
+e.preventDefault;
+$( "#currentImage" ).hide();
+$( "#noImageSelected" ).show();
+$( "#deleteImageField" ).val(1);
+
+
+		});
+
+
+
+     });
+</script>
